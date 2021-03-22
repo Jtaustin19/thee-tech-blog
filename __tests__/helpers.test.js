710c394cb1152d9dfb312e4c4ec27d4a1ee27f69
Josh <jtaustin19@icloud.com>
2021-03-21 21:26:18 -0500

    refactor

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,13 +1,13 @@
 const { format_date, format_plural } = require("../utils/helpers");
 
 test("format_date() returns clear date string", () => {
-    const date = new Date("2021-03-17 13:43:24");
+    const date = new Date("2021-03-19 14:57:04");
 
-    expect(format_date(date)).toBe("3/17/2021");
+    expect(format_date(date)).toBe("3/19/2021");
 });
 
 test("format_plural() returns string(s) correctly pluralized", () => {
 
-    expect(format_plural(1, "Tiger")).toBe("Tiger");
-    expect(format_plural(3, "Lion")).toBe("Lions");
+    expect(format_plural(1, "Teddy Bear")).toBe("Teddy Bear");
+    expect(format_plural(3, "Teddy Bear")).toBe("Teddy Bears");
 });
